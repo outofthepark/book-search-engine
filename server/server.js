a5bee1bb486642ba3aee648fedd5cc3be1740c8d
Allison Harden <harden.allison@gmail.com>
2021-09-10 12:07:47 -0600

    Removed build folder from commit, package changes.

--- server/server.js
+++ server/server.js
@@ -1,36 +1,38 @@
-const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
-
-const {typeDefs, resolvers} = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({ 
-  typeDefs, 
-  resolvers, 
-  context: authMiddleware 
-});
-
-server.applyMiddleware({ app });
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+async function startApolloServer(typeDefs, resolvers, middleware){ 
+
+  const server = new ApolloServer({ typeDefs, resolvers, context: middleware });
+  await server.start();
+  server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+          console.log(`API server running on port ${PORT}!`);
+          // log where we can go to test our GQL API
+          console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        });
+    });
+}
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
+startApolloServer(typeDefs, resolvers, authMiddleware);
